a4c5d35b32cdfd7f3132e9a22eba84cf8a899841
Jairos Nyirenda <jairos.pemphonyirenda@gmail.com>
2022-08-29 12:15:08 +0200

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,65 @@
+import express from 'express';
+import {Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles, getAppPath} from './util/util';
+
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+  const validURL = require('valid-url')
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  //! END @TODO1
+  
+  app.get("/filteredimage?:image_url", async (req: Request, res: Response) => {
+    try 
+    {
+      const { image_url } = req.query;
+      if (!image_url) {
+        return res.status(400).send({ message: 'Image URL is required or malformed' });
+      }
+
+      const filteredpath:string = await filterImageFromURL(image_url)
+      res.status(200).sendFile(filteredpath)
+      await deleteLocalFiles([filteredpath])
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({ message: 'Oops! there\'s an error with our servers, please try again later' });
+    }
+  });
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
---
+++ src/util/util.ts
@@ -0,0 +1,64 @@
+import fs from "fs";
+import Jimp = require("jimp");
+
+// filterImageFromURL
+// helper function to download, filter, and save the filtered image locally
+// returns the absolute path to the local image
+// INPUTS
+//    inputURL: string - a publicly accessible url to an image file
+// RETURNS
+//    an absolute path to a filtered image locally saved file
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const photo = await Jimp.read(inputURL);
+      const outpath =
+        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      await photo
+        .resize(256, 256) // resize
+        .quality(60) // set JPEG quality
+        .greyscale() // set greyscale
+        .write(__dirname + outpath, (img) => {
+          resolve(__dirname + outpath);
+        });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+// deleteLocalFiles
+// helper function to delete files on the local disk
+// useful to cleanup after tasks
+// INPUTS
+//    files: Array<string> an array of absolute paths to files
+export async function deleteLocalFiles(filesPath: Array<string>) {
+  try {
+    for (let filePath of filesPath) {
+    if (fs.existsSync(filePath)) {
+      const rootPath = await getAppPath()
+      const temp = 'src\\util\\tmp\\'
+      const frags = filePath.split('/')
+      const file = frags[(frags.length - 1)];
+      const path = `${rootPath}\\${temp}\\${file}`
+      fs.unlinkSync(path);
+    }
+  }
+  } catch(err) {
+
+  }
+}
+
+export async function getAppPath() {
+  const { dirname } = require('path');
+  const { constants, promises: { access } } = require('fs');
+  
+  for (let path of module.paths) {
+    try {
+      await access(path, constants.F_OK);
+      return dirname(path);
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
